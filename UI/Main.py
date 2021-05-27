ff9cb3f197256e5adf25fdbca190cf7145988ea8
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-27 17:30:08 -0400

    Update Main.py
    remove testing import from main

--- UI/Main.py
+++ UI/Main.py
@@ -1,5 +1,4 @@
 from data.Person import CompanyPeople, GovernmentPeople
-from Testing.Testing import ob
 
 john = CompanyPeople("John", "Helix", "11101970", "CEO", "Voters R Us")
 jay = GovernmentPeople("Jay", "Dump", "06071950", "Governor", "State")
