9a25b669401f2a1f79a85cc7482fa8c4d73fc1f7
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-27 17:20:52 -0400

    Create Main.py
    test CompanyPeople and GovernmentPeople

---
+++ UI/Main.py
@@ -0,0 +1,6 @@
+from data.Person import CompanyPeople, GovernmentPeople
+from Testing.Testing import ob
+
+john = CompanyPeople("John", "Helix", "11101970", "CEO", "Voters R Us")
+jay = GovernmentPeople("Jay", "Dump", "06071950", "Governor", "State")
+
