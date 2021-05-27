e1864b464e184475e6d86784c3ea0895cc2abe47
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-27 17:38:05 -0400

    Update Main.py
    removed extra line

--- UI/Main.py
+++ UI/Main.py
@@ -2,4 +2,3 @@ from data.Person import CompanyPeople, GovernmentPeople
 
 john = CompanyPeople("John", "Helix", "11101970", "CEO", "Voters R Us")
 jay = GovernmentPeople("Jay", "Dump", "06071950", "Governor", "State")
-
