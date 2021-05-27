a1cd2bd330794a47baa58236eede5424ba60e5e5
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-27 17:19:34 -0400

    Update Person.py
    add testing decorators to objects

--- data/Person.py
+++ data/Person.py
@@ -2,8 +2,10 @@
 # Data structure to hold data for all people in the game
 # 5/27/2021
 from dataclasses import dataclass, asdict
+from Testing.Testing import ob
 
 
+# @ob
 @dataclass(frozen=True, order=True)
 class Person:
     first: str
@@ -11,12 +13,14 @@ class Person:
     dob: str
 
 
+@ob
 @dataclass(frozen=True, order=True)
 class CompanyPeople(Person):
     title: str
     company: str
 
 
+@ob
 @dataclass(frozen=True, order=True)
 class GovernmentPeople(Person):
     title: str
