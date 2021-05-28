80d9d86d230812d843d456ab63aa348715c19969
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-28 14:20:53 -0400

    Update Main.py
    import Systems
    test Systems

--- UI/Main.py
+++ UI/Main.py
@@ -1,4 +1,5 @@
-from data.DataMgmt import CompanyPeople, GovernmentPeople, Player
+from data.DataMgmt import CompanyPeople, GovernmentPeople, Player, Systems
 john = CompanyPeople("John", "Helix", "11101970", "CEO", "Voters R Us")
 jay = GovernmentPeople("Jay", "Dump", "06071950", "Governor", "State")
 player1 = Player()
+player_pc = Systems(localhost=1)
