69f70c91ac025ae90157d27a4b814611f240bdbb
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-28 14:15:18 -0400

    Create DataMgmt.py
    add Player, Systems classes

---
+++ data/DataMgmt.py
@@ -0,0 +1,49 @@
+# Ernst Fanfan
+# Data structure to hold data in the game
+# 5/27/2021
+
+from dataclasses import dataclass, field
+from Testing.Testing import ob
+
+############################################
+# Character classes
+# @ob
+@dataclass(frozen=True, order=True)
+class Person:
+    first: str
+    last: str
+    dob: str
+
+
+@ob
+@dataclass(frozen=True, order=True)
+class CompanyPeople(Person):
+    title: str
+    company: str
+
+
+@ob
+@dataclass(frozen=True, order=True)
+class GovernmentPeople(Person):
+    title: str
+    department: str
+
+
+#######################################################
+# Player level and logs
+@ob
+@dataclass()
+class Player:
+    level: int = 1
+    log: list = field(default_factory=list)
+
+
+######################################################
+# Systems
+@dataclass()
+class Systems:
+    localhost: int = 0
+    vote: int = 0
+    state: int = 0
+    mail: int = 0
+    files: list = field(default_factory=list)
