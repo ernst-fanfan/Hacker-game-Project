3019f14ef02fd1215b72f7d8b8e97fe22c5bf4a5
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-28 14:14:16 -0400

    Update Main.py
    test player obj

--- UI/Main.py
+++ UI/Main.py
@@ -1,4 +1,4 @@
-from data.Person import CompanyPeople, GovernmentPeople
-
+from data.DataMgmt import CompanyPeople, GovernmentPeople, Player
 john = CompanyPeople("John", "Helix", "11101970", "CEO", "Voters R Us")
 jay = GovernmentPeople("Jay", "Dump", "06071950", "Governor", "State")
+player1 = Player()
