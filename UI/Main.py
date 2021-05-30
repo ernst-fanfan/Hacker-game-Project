4503f922faf2a309e82184cd9ce8e4410eaff36f
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-29 23:50:39 -0400

    testing

--- UI/Main.py
+++ UI/Main.py
@@ -1,5 +1,20 @@
 from data.DataMgmt import CompanyPeople, GovernmentPeople, Player, Systems
+from dataclasses import asdict
+import UI
+import time
+
 john = CompanyPeople("John", "Helix", "11101970", "CEO", "Voters R Us")
 jay = GovernmentPeople("Jay", "Dump", "06071950", "Governor", "State")
 player1 = Player()
-player_pc = Systems(user_id="admin", password="Password", localhost=1)
+player_pc = Systems(user_id="admin", password="Password", programs=["mail"], files=["one"])
+p = asdict(player_pc)
+print(p)
+
+player_pc.vote = 1
+p = asdict(player_pc)
+cmd = ""
+current_ui = "localhost"
+cmd = UI.cmd_parser(input(f"{current_ui}>>"))
+UI.ui_manager(p, cmd)
+
+
