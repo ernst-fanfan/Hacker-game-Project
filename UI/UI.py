c810ae151585f46b131b3896345194843fbcddc4
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-06-01 16:09:21 -0400

    Update UI.py
    remove old UI
    add cmd_parser to prepare cmd for validation
    add cmd  and arg checker
    add cmd_handler to process cmd and arg and return function

--- UI/UI.py
+++ UI/UI.py
@@ -5,55 +5,40 @@ from Testing.Testing import func_tester
 from dataclasses import asdict
 
 
+######################################################
 # @func_tester
-def cmd_parser(raw_cmd):
-    cmd = ""
-    if "open" in raw_cmd:
-        try:
-            cmd, file = raw_cmd.split()
-        except ValueError:
-            return "no filename"
-        return cmd, file
-    return raw_cmd
+# keyword argument
+def cmd_parser(input_str):
+    try:
+        cmd, arg = input_str.split()
+        return cmd, arg
+    except ValueError:
+        cmd = input_str
+        arg = ""
+        return cmd, arg
 
 
-@func_tester
-def system_manger(current_system):
-    return asdict(current_system)
+# cmd checker
+def is_program_in_system(cmd, programs={}):
+    return programs.get(cmd, "error")
 
 
-@func_tester
-def show_files(files):
-    return "files"
+# arg checker
+def is_arg_in_system(arg, dct):
+    return dct.get(arg, "Error")
 
 
-@func_tester
-def mail_ui(programs):
-    return "mail system"
-
-
-@func_tester
-def vote_ui(programs):
-    return "vote system"
-
-
-@func_tester
-def state_ui(programs):
-    return "state system"
-
-
-def ex_func():
-    return "exiting Program"
-
-
-# @func_tester
-def ui_manager(current_system, cmd):
-    ui_switcher = {
-        "ls": show_files,
-        "mail": mail_ui,
-        "vote": vote_ui,
-        "state": state_ui,
-        "exit": ex_func
-    }
-    func = ui_switcher.get(cmd)
-    func(current_system)
+# cmd handler
+def cmd_handler(input_str, system, user_account=None):
+    cmd, arg = cmd_parser(input_str)
+    program = is_program_in_system(cmd, system.programs)
+    if program.arg == 0:
+        return program.func
+    if program.name == "mail":
+        emails = user_account.emails
+        arg = is_arg_in_system(arg, emails)
+        return program.func(arg)
+    if program.name == "ls":
+        files = user_account.files
+        arg = is_arg_in_system(arg, files)
+        return program.func(arg)
