b8483c7c36d4ff9f8c65656596db0087ace02212
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-06-01 16:06:44 -0400

    Update DataMgmt.py
    streamline System class
    add organization and UserAccount classes
    replace GovernementPeople and CompanyPeople with Employee

--- data/DataMgmt.py
+++ data/DataMgmt.py
@@ -3,7 +3,37 @@
 # 5/27/2021
 
 from dataclasses import dataclass, field
+from datetime import datetime
+
+from typing import List
+
 from Testing.Testing import ob
+
+
+#######################################################
+# Logs player's progress through the story
+# @ob
+@dataclass()
+class Log:
+    date: datetime
+    entree: str
+
+
+#######################################################
+# Stores player level and logs
+# @ob
+@dataclass()
+class Player:
+    level: int = 1
+    logs: list[Log] = field(default_factory=list)
+
+
+######################################################
+# Files
+@dataclass()
+class File:
+    name: str
+    content: str
 
 
 ############################################
@@ -16,43 +46,57 @@ class Person:
     dob: str
 
 
-# @ob
-@dataclass(frozen=True, order=True)
-class CompanyPeople(Person):
-    title: str
-    company: str
-
-
-# @ob
-@dataclass(frozen=True, order=True)
-class GovernmentPeople(Person):
-    title: str
-    department: str
-
-
-#######################################################
-# Player level and logs
-# @ob
+######################################################
+# emails
 @dataclass()
-class Player:
-    level: int = 1
-    log: list = field(default_factory=list)
+class Email:
+    sender: Person
+    content: str
 
 
 ######################################################
 # Systems
 # @ob
 @dataclass()
-class Systems:
+class UserAccount:
     user_id: str
     password: str
-    programs: list = field(default_factory=list)
-    files: list = field(default_factory=list)
+    emails: dict[Email] = field(default_factory=dict)
+    files: dict[File] = field(default_factory=dict)
+
+
+############################################
+# Character extensions
+# @ob
+@dataclass(frozen=True, order=True)
+class Employee(Person):
+    title: str
+    department: str
+    account: UserAccount
 
 
 ######################################################
 # Programs
 @dataclass()
-class Programs:
+class Program:
     name: str
-    files: list = field(default_factory=list)
+    func: str
+    arg: int = 0
+
+
+######################################################
+# Systems
+# @ob
+@dataclass()
+class System:
+    users: list[Employee] = field(default_factory=list)
+    programs: dict[Program] = field(default_factory=dict)
+
+
+######################################################
+# Organizations
+@dataclass()
+class Organizations:
+    name: str
+    employees: list[Employee] = field(default_factory=list)
+    systems: list[System] = field(default_factory=list)
